420590397c06fd4f022e9b194dfe91604d1d6367
Zelda <zelda.katra@gmail.com>
2024-07-12 02:46:47 +0200

    logement start

---
+++ src/components/Slideshow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Slideshow.scss';
+
+const Slideshow = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Slideshow;
--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './Logement.scss';
 import { useParams, useNavigate } from 'react-router-dom';
+import Slideshow from '../components/Slideshow';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Collapse from '../components/Collapse';
 
 const Logement = () => {
     const { id } = useParams();
@@ -19,7 +23,19 @@ const Logement = () => {
 
     return (
         <div>
-
+            <Header />
+            <main className='Logement'>
+                <Slideshow />
+            </main>
+            <div className='LogementCollapses'>
+                <Collapse
+                    title={'Description'}
+                    text={dataID.description} />
+                <Collapse
+                    title={'Équipements'}
+                    text={dataID.equipments} />
+            </div>
+            <Footer />
         </div>
     );
 };
